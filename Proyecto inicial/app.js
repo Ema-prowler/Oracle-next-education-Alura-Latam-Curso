619801bcd6a8dd8c23e5515ee02a59ba5e84c3b8
Emanuel Coletti <emanuelcoletti001@gmail.com>
2024-01-17 15:38:38 -0300

    agregando un alert, prompt y la variable de almacenamiento numeroUsuario

---
+++ Proyecto inicial/app.js
@@ -0,0 +1,13 @@
+/*let numeroSecreto = 6;
+let numeroUsuario = prompt("Me indicas un numero por favor:");
+
+console.log(numeroUsuario);
+
+if (numeroUsuario == numeroSecreto) {
+    alert('Acertaste el numero');
+}
+*/
+//la variable va del lado izquierdo (<)
+//El valor va del lado derecho (>)
+alert('Hola Mundo');
+let numeroUsuario = prompt("Me puedes decir un numero por favor :");
