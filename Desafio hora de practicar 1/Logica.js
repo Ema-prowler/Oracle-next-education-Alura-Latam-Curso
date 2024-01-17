3a6f12f03d84f9ff4ffb4cffecf80e71d6238df7
Emanuel Coletti <emanuelcoletti001@gmail.com>
2024-01-17 16:29:40 -0300

    agregando un console log para mapear los resultados y agregando 3 variables nuevas

--- Desafio hora de practicar 1/Logica.js
+++ Desafio hora de practicar 1/Logica.js
@@ -1,3 +1,6 @@
-alert('¡Bienvenida y bievenido a nuestro sitio web!')
+alert('¡Bienvenida y bievenido a nuestro sitio web!');
 let nombre = "luna";
-console.log(nombre)
+console.log(nombre);
+let edad = 25;
+let numeroDeVentas = 50;
+let saldoDisponible = 1000;
