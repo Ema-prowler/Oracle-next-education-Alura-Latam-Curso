e3a39778b57be5723e48ffef97a437f46e06136e
Emanuel Coletti <emanuelcoletti001@gmail.com>
2024-01-17 16:06:56 -0300

    borranado lineas incesarias, comentando y dando una mejor descripcion al mensaje inicial

--- Proyecto inicial/app.js
+++ Proyecto inicial/app.js
@@ -2,10 +2,10 @@
 //El valor va del lado derecho (>)
 // una llave cerrada {} es un bloque de codigo
 //alert('Hola Mundo');
-let mensajeDeBienvenida = 'Bienvenido a la aplicación';
-alert(mensajeDeBienvenida);
+//let mensajeDeBienvenida = 'Bienvenido a la aplicación';
+alert('Bienvenidos al juego del número secreto');
 
-let numeroUsuario = prompt("Me puedes decir un numero por favor :");
+let numeroUsuario = prompt('indique un numero del entre el 1 y 10');
 let numeroSecreto = 6;
 console.log(numeroUsuario);
 
