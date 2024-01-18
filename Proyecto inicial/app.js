404811a801c503291e5d25592db280bb5727e0d0
Emanuel Coletti <emanuelcoletti001@gmail.com>
2024-01-18 18:38:36 -0300

    algunos que otros cambios y demas

--- Proyecto inicial/app.js
+++ Proyecto inicial/app.js
@@ -3,16 +3,21 @@
 // una llave cerrada {} es un bloque de codigo
 //alert('Hola Mundo');
 //let mensajeDeBienvenida = 'Bienvenido a la aplicación';
-alert('Bienvenidos al juego del número secreto');
+alert("Bienvenidos al juego del número secreto");
 
-let numeroUsuario = prompt('indique un numero del entre el 1 y 10');
+let numeroUsuario = prompt("indique un numero del entre el 1 y 10");
 let numeroSecreto = 6;
 
 console.log(numeroUsuario);
 
 if (numeroUsuario == numeroSecreto) {
-    alert(`adivinaste, el numero es: ${numeroUsuario}`);
+  //                                        ┌-------------------┐
+  alert(`adivinaste, el numero es: ${numeroUsuario}`); //Usando TEMPLATE STRING
+} else {
+  alert(
+    "Lo siento no es el numero secreto" +
+      "el numero que elegiste fue" +
+      numeroUsuario,
+    "pero el numero secreto es: " + numeroSecreto
+  );
 }
-else{
-    alert('Lo siento no es el numero secreto' + 'el numero que elegiste fue' + numeroUsuario, 'pero el numero secreto es: ' + numeroSecreto)
-};
